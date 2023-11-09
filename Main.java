14739afc8a05ca849ba780d68b84f26ce4d7a10f
Eirini Koumarinou <148354894+koumarinou@users.noreply.github.com>
2023-11-09 16:47:17 +0200

    Create main that initialize a list with a size and values specified by command-line arguments, validating the input and ensuring the number of values matches the specified list size

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        if(args.length < 1) { //if no definition is given it displays a message and terminates
+            System.out.println("No definitions were given.To run the program you have to put the size of the list and next to it the times.The execution is terminated.");
+            return;
+        }
+
+        int size; //the first definition is the size of the list
+        try {
+            size = Integer.parseInt(args[0]);
+        } catch(NumberFormatException e) {
+            System.out.println("The first definition: " + args[0] + " must be a number indicating the size of the list");
+            return;
+        }
+
+        if(args.length - 1 != size) { //match times and sizes
+            System.out.println("The stated size of the list does not match the times given. Execution terminated.");
+            return;
+        }
+
+        List<Integer> initialTimes = new ArrayList<>(); //create the list and add the times from args[]
+        for(int i = 1; i < args.length; i++) {
+            try {
+                initialTimes.add(Integer.parseInt(args[i]));
+            } catch(NumberFormatException e) {
+                System.out.println("The definition: " + args[i] + " is not a valid number.");
+                return;
+            }
+        }
